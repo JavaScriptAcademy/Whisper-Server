eddd6df097974dfbbd25ccb0852a7e04358dcadb
yancey1204 <yancey1204@gmail.com>
2016-04-15 10:30:02 +0800

    edit the schema

--- src/services/rooms/rooms-model.js
+++ src/services/rooms/rooms-model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // rooms-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 
@@ -9,7 +9,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const roomsSchema = new Schema({
-  text: { type: String, required: true },
+  name: { type: String },
+  roomImage: { type: String, trim: true },
+  discription:{ type: String },
+  topic: { type: String, required: 'topic is required'},
+  owner: { type: String, required: 'room owner should be assigned'},
+  members: [{
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  }],
+  audios: [{
+    creatorName : { type: String },
+    createdTime: { type: Date, 'default': Date.now },
+    length: { type: Number},
+    voiceType:{ type: String}
+  }],
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
