37e5c9c13a511e2d91535a4a620bdba7ae78be87
CyrusCai <Cyrus_Cai@epam.com>
2016-04-19 10:49:45 +0800

    change user-model

--- src/services/user/user-model.js
+++ src/services/user/user-model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // user-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 
@@ -11,7 +11,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: {type: String, required: true, unique: true},
   password: { type: String, required: true },
-  
+
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
